9d9b06c9207b0a6b6bfd160a49a170a24d257c90
islomalarov <islomalarov@gmail.com>
2024-11-15 11:15:37 +0500

    fix getGCPCredentials

--- src/utils/googleSheets.ts
+++ src/utils/googleSheets.ts
@@ -3,8 +3,8 @@ import { google } from 'googleapis';
 export async function getGoogleSheetData(sheetId: string, range: string) {
   const auth = new google.auth.GoogleAuth({
     credentials: {
-      client_email: process.env.GCP_SERVICE_ACCOUNT_EMAIL,
-      private_key: process.env.GCP_PRIVATE_KEY,
+      client_email: credentials?.client_email,
+      private_key: credentials?.private_key,
     },
     scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
   });
@@ -17,3 +17,18 @@ export async function getGoogleSheetData(sheetId: string, range: string) {
 
   return response.data.values;
 }
+export const getGCPCredentials = () => {
+  // for Vercel, use environment variables
+  return process.env.GCP_PRIVATE_KEY
+    ? {
+        credentials: {
+          client_email: process.env.GCP_SERVICE_ACCOUNT_EMAIL,
+          private_key: process.env.GCP_PRIVATE_KEY,
+        },
+        projectId: process.env.GCP_PROJECT_ID,
+      }
+    : // for local development, use gcloud CLI
+      {};
+};
+
+const { credentials } = getGCPCredentials();
